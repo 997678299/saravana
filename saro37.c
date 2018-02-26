03769779a14fba898439dd9fcedfe0427e718bcc
997678299 <35995734+997678299@users.noreply.github.com>
2018-02-26 16:13:43 +0530

    Create saro37.c

---
+++ saro37.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int x,y,Temp;
+printf("Enter the value of x: ");
+scanf("%d", &x);
+printf("Enter the value of y: ");
+scanf("%d",&y);
+Temp=x;
+x =y;
+y =Temp;
+printf("\nAfter swapping,a =%.2d\n",x);
+printf("\nAfter swapping,b= %.2d\n",y);
+return 0;
+getch();
+}
