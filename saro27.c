7cbe641c00a7513c58c7c4e338797cc0741e17f7
997678299 <35995734+997678299@users.noreply.github.com>
2018-02-26 15:43:07 +0530

    Create saro27.c

---
+++ saro27.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char tmp[16];
+scanf("%s", &tmp);
+
+int d= 0;
+int j=0;
+while(j<strlen(tmp) && d == 0)
+{
+  if(tmp[j] < 57 && tmp[j] > 48)
+   {
+       d = 0;
+    printf("numeric");
+   }
+  else
+  {
+    d= 1;
+    printf("not numeric");
+  }
+  j++;
+  return 0;
+  getch()0;
+}
