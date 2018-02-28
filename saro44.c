496d81380eff532779ebcf19326e6aa320adfef8
997678299 <35995734+997678299@users.noreply.github.com>
2018-02-28 16:18:46 +0530

    Create saro44.c

---
+++ saro44.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("Enter the number :");
+scanf("%d",&a);
+if(a>=1&&a<=10)
+printf("\nyes");
+else
+printf("\nNo");
+getch();
+return 0;
+}
