a4e5f33d990a4922699680872dcd51c1c99ba62d
997678299 <35995734+997678299@users.noreply.github.com>
2018-03-09 13:34:59 +0530

    Create saro67.c

---
+++ saro67.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,n1;
+printf("enter number");
+scanf("%d",&n);
+n=n/10;
+n1=n+1;
+printf("the nearest greatest multiple is %d",n1*10);
+}
+getch();
+return0;
+}
