da4e4ff9664f29035bfbd611c55917ab0fe3422d
997678299 <35995734+997678299@users.noreply.github.com>
2018-03-23 16:00:22 +0530

    Create saro82.c

---
+++ saro82.c
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+double L,B,area;
+cout<<" Enter the lengthg and breadth of farm : "<<endl;
+cin>>L>>B;
+area=L*B;
+cout<<"The area of farm is :";
+cout<<fixed<<setprecision(5)<<area<<endl;
+return 0;
+getch():
+}
